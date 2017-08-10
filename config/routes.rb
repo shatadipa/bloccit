4c10c2cc693339a2533ffbd73a53775449a0ebb7
Shatadipa Chakraborty <shatadipa1986@gmail.com>
2017-08-09 23:57:46 -0700

    CHapter 18 Checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,29 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  
+  # get 'posts/index'
 
-  get 'welcome/about'
+  # get 'posts/show'
+
+  # get 'posts/new'
+
+  # get 'posts/edit'
+
+     resources :posts
+
+
+#   get 'welcome/index'
+
+# get 'welcome/about'
+
+   get 'about' => 'welcome#about'
+   root 'welcome#index'
+
+
 
   get 'welcome/contact'
 
   get 'welcome/faq'
 
-  root 'welcome#index'
+  
   
 end
